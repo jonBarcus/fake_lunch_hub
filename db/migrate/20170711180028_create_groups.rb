00126c044fca51874842d44951d97fd93e5a7893
jonBarcus <barcus.j@gmail.com>
2017-07-11 14:20:48 -0400

    updated migration

--- db/migrate/20170711180028_create_groups.rb
+++ db/migrate/20170711180028_create_groups.rb
@@ -1,9 +1,11 @@
 class CreateGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :groups do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
+
+    add_index :groups, :name, unique: true
   end
 end
