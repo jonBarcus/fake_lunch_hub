58ad9b2624bd45da210cd9c5798c7c56b230d588
jonBarcus <barcus.j@gmail.com>
2017-07-19 12:57:41 -0400

    messing with npm

--- client/src/app/index.module.js
+++ client/src/app/index.module.js
@@ -9,7 +9,7 @@ import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service'
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
-angular.module('fakeLunchHub', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr'])
+angular.module('fakeLunchHub', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'angular-animate'])
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
